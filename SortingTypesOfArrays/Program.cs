131f51500bc6c3544f48dabb269ed5f37cb3d85c
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-04-10 22:45:33 +0200

    HEAP TRY 1

--- SortingTypesOfArrays/Program.cs
+++ SortingTypesOfArrays/Program.cs
@@ -43,7 +43,48 @@ namespace Straight_Selection_and_Insertion_Sort
 
 
 
+       //-------HEAP---::START::------------------------------------------------------------------------------------------------------------------------------------------
+   
+    static void HeapSort(int[] array)
+    {
+        var length = array.Length;
+        for (int i = length / 2 - 1; i >= 0; i--)
+        {
+            Heapify(array, length, i);
+        }
+        for (int i = length - 1; i >= 0; i--)
+        {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            Heapify(array, i, 0);
+        }
+    }
 
+    //Rebuilds the heap
+    static void Heapify(int[] array, int length, int i)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < length && array[left] > array[largest])
+        {
+            largest = left;
+        }
+        if (right < length && array[right] > array[largest])
+        {
+            largest = right;
+        }
+        if (largest != i)
+        {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+            Heapify(array, length, largest);
+        }
+    }
+
+       //-------HEAP---::END::------------------------------------------------------------------------------------------------------------------------------------------
 
         //-----------Merege Sort--:::START::--------------------------------------------------------------------------------------------------------------------------------
 
@@ -128,7 +169,11 @@ namespace Straight_Selection_and_Insertion_Sort
 
         //--Quick Sort----::End::------------------------------------------------------------------------------------------------------------------------------------------------------
 
+        public static void HeapSort()
+        {
 
+
+        }
 
 
 
